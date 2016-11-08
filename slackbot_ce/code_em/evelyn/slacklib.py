2981f93a721069687d1669cde20d2d0c794bfe12
Evelyn Fite <Evypow@verizon.net>
2016-11-08 18:52:57 -0500

    Evelyn added command1
    Gave robot name Piebot.

--- slackbot_ce/code_em/evelyn/slacklib.py
+++ slackbot_ce/code_em/evelyn/slacklib.py
@@ -1,5 +1,5 @@
 # Put your commands here
-COMMAND1 = ""
+COMMAND1 = "What is your name?
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,7 +9,7 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "I'm Piebot! Im a awesome robot who loves pie!"
         
     return response
 
